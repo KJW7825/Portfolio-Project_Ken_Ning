8dbad2c53120a8f28d7abb6c8c61decac2cf6e75
KJW7825 <kening717@gmail.com>
2026-03-10 01:40:35 -0400

    Update PIDControllerKernel.java

--- src/components/pidcontroller/PIDControllerKernel.java
+++ src/components/pidcontroller/PIDControllerKernel.java
@@ -3,19 +3,19 @@ package components.pidcontroller;
 import components.standard.Standard;
 
 /**
- * The Kernel Interface for the PID Controller.
- * * @author Ken Ning
+ * The Kernel Interface for the PID Controller. * @author Ken Ning
  */
 public interface PIDControllerKernel extends Standard<PIDController> {
 
     /**
      * Sets the three main tuning parameters (Gains) for the controller.
-     *  @param p
-     * The Proportional gain - reacts to the CURRENT error
-     *  @param i
-     * The Integral gain - reacts to the sum of PAST errors
-     *  @param d
-     * The Derivative gain - predicts FUTURE error trends
+     * 
+     * @param p
+     *            The Proportional gain - reacts to the CURRENT error
+     * @param i
+     *            The Integral gain - reacts to the sum of PAST errors
+     * @param d
+     *            The Derivative gain - predicts FUTURE error trends
      * @updates this
      * @requires p >= 0 and i >= 0 and d >= 0
      * @ensures this.kp = p and this.ki = i and this.kd = d
@@ -24,13 +24,14 @@ public interface PIDControllerKernel extends Standard<PIDController> {
 
     /**
      * Calculates the control output based on the current system error.
-     *  @param currentError
-     * The gap between the target value and the actual value
+     * 
+     * @param currentError
+     *            The gap between the target value and the actual value
      * @return The calculated action value to be applied to the system
      * @updates this
-     * @ensures calculateOutput = (this.kp * currentError) +
-     * (this.ki * (this.integralSum + currentError)) +
-     * (this.kd * (currentError - this.lastError))
+     * @ensures calculateOutput = (this.kp * currentError) + (this.ki *
+     *          (this.integralSum + currentError)) + (this.kd * (currentError -
+     *          this.lastError))
      */
     double calculateOutput(double currentError);
 
